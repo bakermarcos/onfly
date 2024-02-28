b44664fdddf55c407dbc35039afea977c0a8dfc4
Marcos Baker <bakermarcos52@gmail.com>
2024-02-28 16:11:15 -0300

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:onfly/firebase_options.dart';
 import 'package:onfly/presentation/login/screens/login_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
